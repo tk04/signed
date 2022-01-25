dbfb2ac6491c66274bed886213b346de0c7ca304
taloufi <taloufi@iu.edu>
2022-01-25 16:15:08 -0500

    redesigned landing page

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -3,12 +3,20 @@ import classes from "./Header.module.css";
 const Header = () => {
   return (
     <header className={classes.header}>
-      <p className={classes.logo}>Signed</p>
+      <p className={classes.logo} className="text-2xl font-semibold">
+        Signed
+      </p>
       <nav className={classes["main-nav"]}>
         <ul className={classes.ul}>
-          <li className={classes.list}>test1</li>
-          <li className={classes.list}>test2</li>
-          <li className={classes.list}>test3</li>
+          <li className={classes.list} className=" p-2 rounded-full  px-8">
+            <button>Login</button>
+          </li>
+          <li
+            className={classes.list}
+            className="bg-sky-400 p-2 rounded-full text-white px-8 hover:bg-sky-700"
+          >
+            <button>Get started</button>
+          </li>
         </ul>
       </nav>
     </header>
