e08ee2059055e1723636bb10bbe7c6a8be06b247
taloufi <taloufi@iu.edu>
2022-01-25 23:11:52 -0500

    adjusted footer styling + responsiveness

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,30 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Skeleton } from "@mui/material";
-import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/router";
-import { authActions, getUserData } from "../store/auth-slice";
 import Header from "../components/Header";
-import CreatePosts from "../components/CreatePosts";
 import MainIMG from "../public/frontimg.jpeg";
+import classes from "../styles/Home.module.css";
 export default function Home() {
-  const dispatch = useDispatch();
   const router = useRouter();
-  const isAuth = useSelector((state) => state.auth.isAuth);
-  const logoutHandler = () => {
-    dispatch(authActions.logout());
-  };
-  const loginHandler = () => {
-    router.push("/users/sign-in");
-  };
-  const getData = () => {
-    // dispatch(getUserData());
-    router.push("/users/turki");
-  };
 
   return (
-    <div className="bg-slate-50 h-screen w-screen  m-0 p-0">
+    <html className="bg-slate-50 overflow-x-hidden h-screen">
       <Head>
         <title>Signed --Homepage</title>
         <meta
@@ -32,77 +16,77 @@ export default function Home() {
           content="A social plateform that helps you grow your career and network with like-minded individuals"
         />
       </Head>
-      <div style={{ margin: "0 4vw", paddingTop: "4vw" }}>
-        <Header />
-        <div className="flex items-center justify-evenly  ">
-          <div className="ml-20">
-            <p
-              className=" -mt-20 font-semibold"
-              style={{
-                fontSize: "100px",
-                // fontFamily: "'Rubik', sans-serif",
-                // marginLeft: "10rem",
-                // marginRight: "35rem",
-              }}
-            >
-              Grow Your
-              <br /> Brand
-            </p>
-            <p
-              className="mt-2 text-gray-500"
-              style={{ width: "250px", fontSize: "20px" }}
-            >
-              Grow you&apos;re career, showcase your skills and experience, and
-              connect with like-minded people
-            </p>
-            <button
-              className="border-2 border-sky-400 p-4  text-black mt-6 w-72 font-bold text-xl"
-              onClick={() => router.push("/users/sign-up")}
-            >
-              Sign Up <span className="text-sm">--it&apos;s free</span>
-            </button>
-          </div>
-          <div>
-            <Image src={MainIMG} width={700} height={700} objectFit="contain" />
+      <div className="flex flex-col content-between bg-slate-50  ">
+        <div style={{ margin: "0 4vw", paddingTop: "4vw" }}>
+          <Header />
+          <div className="flex items-center justify-evenly    ">
+            <div className="ml-20">
+              <p
+                className=" -mt-20 font-semibold"
+                style={{
+                  fontSize: "100px",
+                  // fontFamily: "'Rubik', sans-serif",
+                  // marginLeft: "10rem",
+                  // marginRight: "35rem",
+                }}
+              >
+                Grow Your
+                <br /> Brand
+              </p>
+              <p
+                className="mt-2 text-gray-500"
+                style={{ width: "250px", fontSize: "20px" }}
+              >
+                Grow you&apos;re career, showcase your skills and experience,
+                and connect with like-minded people
+              </p>
+              <button
+                className="border-2 border-sky-400 p-4  text-black mt-6 w-72 font-bold text-xl"
+                onClick={() => router.push("/users/sign-up")}
+              >
+                Sign Up <span className="text-sm">--it&apos;s free</span>
+              </button>
+            </div>
+            <div className="hidden sm:block">
+              <Image
+                src={MainIMG}
+                width={700}
+                height={700}
+                objectFit="contain"
+              />
+            </div>
           </div>
         </div>
-        {/* {isAuth ? (
-        <>
-          <button onClick={logoutHandler}>Logout</button>
-          <button onClick={getData}>getUserdata</button>
-        </>
-      ) : (
-        <button onClick={loginHandler}>Login</button>
-      )} */}
+        <footer
+          className={classes.footer}
+          style={{
+            bottom: "0",
+            fontSize: "12px",
+            overflowX: "hidden",
+
+            overflowWrap: "break-word",
+          }}
+        >
+          <p>About</p>
+          <p>Help Center</p>
+          <p>Terms of Service</p>
+          <p>Private Policy</p>
+          <p>Cookie Policy</p>
+          <p>Accessibility</p>
+          <p>Ads info</p>
+          <p>Blog</p>
+          <p>Status</p>
+          <p>Careers</p>
+          <p>Brand Resources</p>
+          <p>Advertising</p>
+          <p>Marketing</p>
+          <p>Signed for Business</p>
+          <p>Developers</p>
+          <p>Directory</p>
+          <p>Settings</p>
+          <p>&copy; Signed, inc.</p>
+        </footer>
       </div>
-      <footer
-        className="flex space-x-4 justify-center w-full absolute flex-wrap cursor-pointer"
-        style={{
-          bottom: "5px",
-          fontSize: "12px",
-          overflowX: "hidden",
-          overflowWrap: "break-word",
-        }}
-      >
-        <p>About</p>
-        <p>Help Center</p>
-        <p>Terms of Service</p>
-        <p>Private Policy</p>
-        <p>Cookie Policy</p>
-        <p>Accessibility</p>
-        <p>Ads info</p>
-        <p>Blog</p>
-        <p>Status</p>
-        <p>Careers</p>
-        <p>Brand Resources</p>
-        <p>Advertising</p>
-        <p>Marketing</p>
-        <p>Signed for Business</p>
-        <p>Developers</p>
-        <p>Directory</p>
-        <p>Settings</p>
-        <p>&copy; Signed, inc.</p>
-      </footer>
-    </div>
+    </html>
   );
 }
